2851d67a830c42f34290c2afa9cb659a3f922477
Fadlurahman Lawal <70358533+kingfad@users.noreply.github.com>
2022-08-08 03:07:12 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-"""Contains the City model"""
+"""
+Class City that inherits from BaseModel
+"""
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Implements the City class"""
+    """
+    Class City that inherits from BaseModel
+    Attributes:
+        state_id(str): state id
+        name(str): city name
+    """
     state_id = ""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Constructor of City"""
+        super().__init__(*args, **kwargs)
